5df319b94358fa14456e6feed505fb0fcc9d450f
Dharmendra Kumar <dharmendra@investwellonline.com>
2026-09-09 16:52:14 +0530

    no message

--- Package.swift
+++ Package.swift
@@ -10,18 +10,13 @@ let package = Package(
         .library(
             name: "MintFrameworks",
             targets: ["MintFrameworks"]
+        ),
+        .library(
+            name: "VoltFramework",
+            targets: ["VoltFramework"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/danielgindi/Charts.git", from: "5.1.0"),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", from: "8.0.3"),
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.6.1"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.21.7"),
-        .package(url: "https://github.com/jonkykong/SideMenu.git", from: "6.5.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
-        .package(url: "https://github.com/TimOliver/TOCropViewController.git", from: "3.2.0"),
-        .package(url: "https://github.com/Yummypets/YPImagePicker.git", from: "5.4.0")
-    ],
+    dependencies: [],
     targets: [
         .binaryTarget(
             name: "MintFrameworks",
