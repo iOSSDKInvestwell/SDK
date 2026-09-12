556f552d6f8d8b9851023d8c8883d2e9b27ad5e3
Dharmendra Kumar <dharmendra@investwellonline.com>
2026-09-12 21:40:50 +0530

    no message

--- Package.swift
+++ Package.swift
@@ -31,12 +31,12 @@ let package = Package(
         .binaryTarget(
             name: "MintFrameworksBinary",
             url: "https://github.com/iOSSDKInvestwell/SDK/releases/download/1.0.3/MintFrameworks.xcframework.zip",
-            checksum: "a9f77a73ea16988068856971ec847133bd91480e7d0c45be4034adc3400f09e8"
+            checksum: "7c3a32f5611b161a75e8c562f4097da8fd78cd71daf62443e11040ca22bc5dbe"
         ),
         .binaryTarget(
             name: "VoltFrameworkBinary",
             url: "https://github.com/iOSSDKInvestwell/SDK/releases/download/1.0.3/VoltFramework.xcframework.zip",
-            checksum: "f3b7ce4b9f6507fd104960692b90c75d92306387cce33425a93a8ba3a20ca784"
+            checksum: "529d0a92b9734934c4d66d3d3993c90c8d1fcaa304551a94fd3b3b0e64d36c88"
         ),
         .target(
             name: "MintFrameworksWrapper",
